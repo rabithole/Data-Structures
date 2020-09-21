e64926d148a05703e8f4337c99a4a60fb98b2a52
Robert Kulp <pedalinon@gmail.com>
2020-09-21 14:03:56 -0700

    halfway done

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -1,10 +1,10 @@
-print('2d list')
+print('2d list\n')
 
 def printDl():
     currentNode = dl.head
     while True:
         if currentNode is not None:
-            print('DL:', currentNode.value, '\n--->', 'LENGTH', dl.length, '\n--->', 'HEAD:', dl.head.value, '\n--->', 'TAIL:', dl.tail.value, '\n'),
+            print('---> DL Value:', currentNode.value, '\n--->', 'LENGTH', dl.length, '\n--->', 'HEAD:', dl.head.value, '\n--->', 'TAIL:', dl.tail.value, '\n'),
             currentNode = currentNode.next
         else:
             # print(dl.tail.value, '--->')
@@ -42,11 +42,11 @@ class DoublyLinkedList:
         if self.head is None and self.tail is None:
             self.head = newNode
             self.tail = newNode
-            print('\nadd to head')
         else:
-            print('else in add to head\n')
-            self.head.prev =  self.head.next
+            newNode.next = self.head
+            self.head.prev = newNode
             self.head = newNode
+            # self.head.prev = None
         
     """
     Removes the List's current head node, making the
@@ -54,30 +54,62 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
-            
+        if self.head == None:
+            print('No Head')
+        else:
+            delVal = self.head
+            # self.head.next.prev = None
+            self.head = self.head.next
+        return delVal.value
     """
     Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
-        pass
-            
+        newNode = ListNode(value)
+        if self.tail == None:
+            # print('if add to tail')
+            self.head = newNode
+            self.tail = newNode
+        else:
+            # print('else add to tail')
+            newNode.prev = self.tail
+            self.tail.next = newNode
+            self.tail = newNode
+        return self.tail.value
     """
     Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
+        if self.tail == None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            delVal = self.tail
+            self.tail = self.tail.prev
+            self.tail.next = None
+            # print(delVal.value, self.tail.next.value)
+        return delVal.value
+            
+
             
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
-        pass
+        current = self.head.value
+        if current == node:
+            print('found it at the head')
+        while current:
+            if current == node:
+                current.prev.next = current.next
+                current.next.prev = current.prev
+            current = current.next
+
         
     """
     Removes the input node from its current spot in the 
@@ -101,16 +133,41 @@ class DoublyLinkedList:
         pass
 
     def printList(self):
-        while self.head:
-            print('Print', self.head.value)
-            self.head = self.head.next
+        currentNode = self.head
+        # print('Head', self.head.prev)
+        while currentNode:
+            print(currentNode.value)
+            currentNode = currentNode.next
+        else:
+            print('\nEnd of list \n')
+            
+            
 
 
 dl = DoublyLinkedList()
 dl.add_to_head(1)
 dl.add_to_head(2)
 dl.add_to_head(3)
-dl.add_to_head(14)
+dl.add_to_head(4)
+dl.add_to_head(5)
+dl.add_to_head(6)
+dl.add_to_head('what')
+dl.add_to_head('where')
+
+# dl.remove_from_head()
+
+dl.add_to_tail('the end')
+dl.add_to_tail('the next ending')
+dl.add_to_tail('one more end')
+
+dl.remove_from_tail()
+dl.remove_from_tail()
+# dl.remove_from_tail()
+# dl.remove_from_tail()
+# dl.remove_from_tail()
+
 
 # printDl()
+
+dl.move_to_front('where')
 dl.printList()
